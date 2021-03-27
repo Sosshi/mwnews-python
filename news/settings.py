f256552133f0af59e8dfbef3725e3fafad3d6f8f
Sosshi <43267903+Sosshi@users.noreply.github.com>
2021-03-27 14:15:35 +0200

    static issues

--- news/settings.py
+++ news/settings.py
@@ -120,13 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Extra lookup directories for collectstatic to find static files
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
 #  Add configuration for static files storage using whitenoise
