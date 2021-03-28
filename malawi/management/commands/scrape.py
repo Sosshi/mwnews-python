483975aea48bebcd8decff18ada96e0d6c1c213f
Sosshi <43267903+Sosshi@users.noreply.github.com>
2021-03-28 07:56:47 +0200

    added scrape

---
+++ malawi/management/commands/scrape.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...news_sites import run_code
+
+
+class Command(BaseCommand):
+    help = "Scrapes data from the internet"
+
+    def handle(self):
+        try:
+            run_code()
+        except:
+            print("Failed to run scrapper")
+            return
+
+        self.stdout.write(self.style.SUCCESS("Successfully printed all Book titles"))
+        return
