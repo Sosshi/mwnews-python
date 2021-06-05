a91052c83d8d0250a64f1879747e5c080947b24f
Sosshi <43267903+Sosshi@users.noreply.github.com>
2021-06-05 07:43:07 +0200

    time for api's

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from malawi.api.views import NewsViewset
+
+router = DefaultRouter()
+
+router.register(r"news", NewsViewset, basename="news")
+
+
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("malawi.urls"))]
