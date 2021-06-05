da2df62b0520bf837c1af76f267e744d69d45a9e
Sosshi <43267903+Sosshi@users.noreply.github.com>
2021-06-05 07:50:46 +0200

    code

--- news/settings.py
+++ news/settings.py
@@ -141,3 +141,5 @@ STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
