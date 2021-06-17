6d73a09b96260d332eb087db57ff4f2f764a026f
sosshi <symonmwenex@gmail.com>
2021-06-17 17:11:25 +0200

    added new filter

--- malawi/api/views.py
+++ malawi/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from ..models import News
 from .serializers import NewsSerializers
+from url_filter.integrations.drf import DjangoFilterBackend
 
 class NewsViewset(viewsets.ModelViewSet):
     queryset = News.objects.order_by("-date")
     serializer_class = NewsSerializers
-    search_fields = ["=heading", "=description"]
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['heading', 'description']
