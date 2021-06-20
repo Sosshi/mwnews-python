b3f629001a3485a811687baa911fae08e6051971
sosshi <symonmwenex@gmail.com>
2021-06-20 17:07:17 +0200

    modified the model viewset to a read only model viewset

--- malawi/api/views.py
+++ malawi/api/views.py
@@ -4,7 +4,7 @@ from .serializers import NewsSerializers
 from url_filter.integrations.drf import DjangoFilterBackend
 
 
-class NewsViewset(viewsets.ModelViewSet):
+class NewsViewset(viewsets.ReadOnlyModelViewSet):
     queryset = News.objects.order_by("-date")
     serializer_class = NewsSerializers
     filter_backends = [DjangoFilterBackend]
